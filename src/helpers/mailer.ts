37b3758f78a345039318e0e102226e8a3fdb381f
Tarun_Silam <tarun79767@gmail.com>
2025-01-08 12:25:59 +0530

    made changes in mailer

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -28,7 +28,8 @@ export default async function sendEmail({email,emailType,userId}:any){
             from:'tarun79767@gmail.com',
             to:email,
             subject:emailType==="VERIFY"?"Verify your email":"Reset your password",
-            html:`<p>Click <a href=${process.env.DOMAIN}/${emailType==="VERIFY"?"verifyemail":"resetpassword"}?token=${hashedToken}&id=${hashedEmail} >here</a> to ${emailType==="VERIFY"?"Verify your email":"Reset your password"} </p>`
+            html:`<p>Click <a href=${process.env.DOMAIN}/${emailType==="VERIFY"?"verifyemail":"resetpassword"}?token=${hashedToken}&id=${hashedEmail} >here</a> to ${emailType==="VERIFY"?"Verify your email":"Reset your password"} </p>
+            <p>link=${process.env.DOMAIN}/${emailType==="VERIFY"?"verifyemail":"resetpassword"}?token=${hashedToken}&id=${hashedEmail} >here</a> to ${emailType==="VERIFY"?"Verify your email":"Reset your password"}`
         }
         const mailResponse=await transport.sendMail(mailOptions);
         return mailResponse;
