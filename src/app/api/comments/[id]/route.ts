fb2eb0c17a3a60a7874aaed3f64d95ce5ab7d71d
Tarun_Silam <tarun79767@gmail.com>
2025-01-14 18:19:16 +0530

    .

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -1,21 +1,25 @@
 import { connect } from '@/dbConfig/dbConfig';
 import { NextResponse, NextRequest } from 'next/server';
 import Comment from '@/models/commentModel';
+
 connect();
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const { id } = await params;
-        if (!id) {
-            return NextResponse.json({ error: "Blog ID is required" }, { status: 400 });
-        }
-        const comments = await Comment.find({ blogId: id }).populate('createdBy');
-        console.log(comments);
-        if (!comments || comments.length === 0) {
-            return NextResponse.json({ message: "No comments found" }, { status: 404 });
-        }
-        return NextResponse.json({ message: "Comments fetched successfully", comments }, { status: 200 });
-    } catch (error: any) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
+export async function GET(request: NextRequest) {
+  try {
+    const url = new URL(request.url);
+    const blogId = url.pathname.split('/').pop();
+
+    if (!blogId) {
+      return NextResponse.json({ error: "Blog ID is required" }, { status: 400 });
     }
+
+    const comments = await Comment.find({ blogId }).populate('createdBy');
+    if (!comments || comments.length === 0) {
+      return NextResponse.json({ message: "No comments found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Comments fetched successfully", comments }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
